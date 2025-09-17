6f69ef371395e7562a639af1b5dff0f289788347
Airton-jr-cell <airtonsantosjunior20@gmail.com>
2025-09-16 21:40:10 -0300

    a

--- Prof Ana Paula/Desenvolvimento Front-End/16.09.25/script.js
+++ Prof Ana Paula/Desenvolvimento Front-End/16.09.25/script.js
@@ -2,10 +2,27 @@ const botao = document.getElementById("btnvalidar");
 
 console.log(botao);
 botao.addEventListener('click', ()=>{
-    // alert('botão clicado');
-    let numero = document.getElementById("txtnumero1").value;
-    if (numero % 2 == 0) 
-        document.getElementById("txtmensagem").innerText = "O número é par";
-     else 
-        document.getElementById("txtmensagem").innerText = "O número é ímpar";
+    
+    let numero = document.getElementById("txtnumero1").value.trim();
+
+    if(numero != '')
+    {
+        if (numero % 2 == 0) {
+            document.getElementById("txtmensagem").innerText = "O número é par";
+            document.getElementById("txtmensagem").style.color = "blue";
+        } else {
+            document.getElementById("txtmensagem").innerText = "O número é ímpar";
+            document.getElementById("txtmensagem").style.color = "red";
+        }
+            
+        } else
+             {
+            document.getElementById("txtmensagem").innerText = "insira um numero.";
+            document.getElementById("txtnumero1").focus();
+            document.getElementById("txtnumero1").style.backgroundColor= "green";
+        }
     });
+
+
+
+
