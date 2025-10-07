f2d9c4d811d4fde86f8f19145d358a4a8495cdb8
Airton-jr-cell <airtonsantosjunior20@gmail.com>
2025-10-06 22:10:37 -0300

    a

--- Prof Alisson Kuhn/Resolução Estruturada de Problemas/Matriz/Ex10.c
+++ Prof Alisson Kuhn/Resolução Estruturada de Problemas/Matriz/Ex10.c
@@ -8,11 +8,27 @@
 //criar a matriz T que é a matriz transposta de A; mostrar as matrizes organizadas.
 
 void matriz(int mat[TL][TC]){
-	
+	int l,c;
+	for(l=0;l<TL;l++){
+		for(c=0;c<TC;c++){
+			printf("Matriz[%d][%d]= ", l,c);
+				scanf("%d", &mat[l][c]);
+		}
+	}
+}
+
+void organizar(int mat[TL][TC]){
+		for(l=0;l<TL;l++){
+		for(c=0;c<TC;c++){
+			printf("Matriz[%d][%d]= ", l,c);
+				scanf("%d", &mat[l][c]);
+		}
+	}
 }
 
 
 void main(){
 	int mat[TL][TC];
 	matriz(mat);
+	organizar(mat);
 }
